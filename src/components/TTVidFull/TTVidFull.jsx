3faee91753092b978b7e2f3319add5221890f16c
Nate Morgan <nathanielrmorgan@gmail.com>
2022-10-24 11:31:05 -0400

    add tiktok full screen component

---
+++ src/components/TTVidFull/TTVidFull.jsx
@@ -0,0 +1,48 @@
+const TTVidFull = (props) => {
+  const iframe_container = {
+    left: 0,
+    width: "323px",
+    height: "580px",
+    position: "relative"
+  }
+
+    const iframe = {
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      position: "relative",
+      border: 0,
+      objectFit:"fill"
+    }
+
+    console.log(`https://www.tiktok.com/embed/${props.vidID}`)
+  
+  return (
+      <div className="embeded-tok">
+        <div style={iframe_container}>
+          <iframe
+            title="TikTok"
+            src={`https://www.tiktok.com/embed/${props.vidID}`}
+            style={iframe}
+            allowFullScreen
+            scrolling="no"
+            allow="encrypted-media;"
+            />
+        </div>
+        <br />
+        <div>
+          <button>Find on Map</button>
+        </div>
+        <br />
+        <div>
+          <button>Prev</button>
+          <button>Next</button>
+        </div>
+      </div>
+
+
+  );
+};
+
+export default TTVidFull;
