5e2711fd94cbdf065e6982d35aa99f245ae735f3
Damian <bzdyrad@gmail.com>
2022-10-26 11:52:22 -0400

    simplify adding new restaurant

--- src/pages/NewRestaurant/NewRestaurant.jsx
+++ src/pages/NewRestaurant/NewRestaurant.jsx
@@ -1,87 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const NewRestaurant = (props) => {
-  const [form, setForm] = useState({
-    name: '',
-    latitude: '',
-    longitude: '',
-    website: '',
-    cuisineType: '',
-    tags: '',
-  });
-
-  const handleChange = ({ target }) => {
-    setForm({ ...form, [target.name]: target.value });
-  };
+  const [url, setUrl] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.handleAddRestaurant(form);
+    props.handleAddRestaurant(url);
   };
 
   return (
     <div>
       <h3> Add Restaurant </h3>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name-input">Name:</label>
         <input
-          required
           type="text"
-          name="name"
-          id="name-input"
-          value={form.name}
-          placeholder="Restaurant Name"
-          onChange={handleChange}
-        />
-        <label htmlFor="latitude-input">Latitude:</label>
-        <input
-          required
-          type="text"
-          name="latitude"
-          id="latitude-input"
-          value={form.latitude}
-          placeholder="Latitude"
-          onChange={handleChange}
-        />
-        <label htmlFor="longitude-input">Longitude:</label>
-        <input
-          required
-          type="text"
-          name="longitude"
-          id="longitude-input"
-          value={form.longitude}
-          placeholder="Longitude"
-          onChange={handleChange}
-        />
-        <label htmlFor="website-input">Website:</label>
-        <input
-          required
-          type="text"
-          name="website"
-          id="website-input"
-          value={form.website}
-          placeholder="Website URL"
-          onChange={handleChange}
-        />
-        <label htmlFor="cuisine-type-input">Cuisine Type:</label>
-        <input
-          required
-          type="text"
-          name="cuisineType"
-          id="cuisine-type-input"
-          value={form.cuisineType}
-          placeholder="Cuisine Type"
-          onChange={handleChange}
-        />
-        <label htmlFor="tags-input">Tags:</label>
-        <input
-          required
-          type="text"
-          name="tags"
-          id="tags-input"
-          value={form.tags}
-          placeholder="Tag"
-          onChange={handleChange}
+          name="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
         />
         <button type="submit">Add</button>
       </form>
