78a119d5baf764fcfcee4a3143e5e5ece2ddd71e
Nate Morgan <nathanielrmorgan@gmail.com>
2022-10-27 13:51:28 -0400

    new visual for when someone doesn't have any visited restaurants and they check their visited page

--- src/pages/Visited/Visited.jsx
+++ src/pages/Visited/Visited.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import RestaurantCardRow from "../../components/RestaurantCardRow/RestaurantCardRow";
+import tot from "../../assets/tot.png";
 
 const Visited = (props) => {
   const location = useLocation()
@@ -12,11 +13,22 @@ const Visited = (props) => {
   return ( 
     <div>
       <h1>Visited Restaurants</h1>
-      <ul>
-        <RestaurantCardRow title="Most Recent" restaurants={mostRecent}/>
-        <RestaurantCardRow title="Top Rated" restaurants={topRated}/>
-        <RestaurantCardRow title="ToT Favorites" restaurants={totFavorites}/>
-      </ul>
+      {(visited.length === 0) ? (
+        <>
+          <br />
+          <h4 style={{textAlign:"center"}}> You have visited no restaurants </h4>
+          <img src={tot} alt="ToT" style={{width:"95vw"}} />
+          <h4 style={{textAlign:"center"}}> Add restaurants to your visited<br/> by adding a rating on a restaurant details page </h4>
+        </>
+      ):(
+        <ul>
+          <RestaurantCardRow title="Most Recent" restaurants={mostRecent}/>
+          <RestaurantCardRow title="Top Rated" restaurants={topRated}/>
+          <RestaurantCardRow title="ToT Favorites" restaurants={totFavorites}/>
+        </ul>
+      )}
+
+
     </div>
   );
 }
