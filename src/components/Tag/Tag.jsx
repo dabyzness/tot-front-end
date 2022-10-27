c1c5b398c50caea96bfb927f762570435f70f239
Damian <bzdyrad@gmail.com>
2022-10-26 23:46:29 -0400

    add a searchByTag component

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+
+const Tag = ({ name, searchTerm, setSearchTerm }) => {
+  const buttonRef = useRef();
+  return (
+    <button
+      ref={buttonRef}
+      type="button"
+      className="btn btn-primary"
+      data-bs-toggle="button"
+      autoComplete="off"
+      style={{
+        margin: ".25rem",
+        width: "",
+        borderRadius: "20px",
+      }}
+      onClick={() => {
+        if (buttonRef.current.ariaPressed === "true") {
+          setSearchTerm(searchTerm + `${name},`);
+          buttonRef.current.textContent = `${name} \u2713`;
+        } else {
+          setSearchTerm(searchTerm.replace(`${name},`, ""));
+          buttonRef.current.textContent = `${name}`;
+        }
+      }}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default Tag;
